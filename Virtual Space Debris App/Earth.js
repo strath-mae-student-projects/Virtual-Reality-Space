9212a6405db3dc5b0b61d682c22280c90f607d5f
KevinCampbel <66116091+KevinCampbel@users.noreply.github.com>
2020-06-19 17:02:57 +0100

    Update Earth.js

--- Virtual Space Debris App/Earth.js
+++ Virtual Space Debris App/Earth.js
@@ -1,7 +1,7 @@
 // Create The Earth For Space Debris To Orbit
 
 // Create Sphere
-var geometry   = new THREE.SphereGeometry(1000, 32, 32)
+var geometry   = new THREE.SphereGeometry(50, 32, 32)
 var material  = new THREE.MeshPhongMaterial()
 var earthMesh = new THREE.Mesh( geometry, material )
 scene.add(earthMesh)
@@ -11,14 +11,14 @@ material.map    = THREE.ImageUtils.loadTexture('Earth/Noclouds.jpg')
 
 // Add Bump Map - Produces Height
 material.bumpMap    = THREE.ImageUtils.loadTexture('Earth/Bump.jpg')
-material.bumpScale = 10
+material.bumpScale = 5
 
 // Create Specularity of Sea
 material.specularMap    = THREE.ImageUtils.loadTexture('Earth/Specular.jpg')
 material.specular  = new THREE.Color('grey')
 
 // Add Clouds
-var geometry   = new THREE.SphereGeometry(1005, 32, 32)
+var geometry   = new THREE.SphereGeometry(0.51, 32, 32)
 var material  = new THREE.MeshPhongMaterial({
   map     : new THREE.Texture(canvasCloud),
   side        : THREE.DoubleSide,
@@ -28,3 +28,11 @@ var material  = new THREE.MeshPhongMaterial({
 })
 var cloudMesh = new THREE.Mesh(geometry, material)
 earthMesh.add(cloudMesh)
+
+function animate() 
+{
+  requestAnimationFrame( animate );
+  earthMesh.rotation.y += 0.01;
+  renderer.render( scene, camera );
+}
+animate( );
