b8e8fcff6963d64c350e4c2fcb157158bf659509
KevinCampbel <66116091+KevinCampbel@users.noreply.github.com>
2020-07-05 15:54:25 +0100

    Update Earth.js

--- Virtual Space Debris App/Earth.js
+++ Virtual Space Debris App/Earth.js
@@ -1,38 +1,34 @@
 // Create The Earth For Space Debris To Orbit
 
+// Add Light
+scene.add(new THREE.AmbientLight(0x333333));
+
+var light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(500, 300, 500);
+scene.add(light);
+
 // Create Sphere
-var geometry   = new THREE.SphereGeometry(6371, 64, 64)
-var material  = new THREE.MeshPhongMaterial()
+var geometry   = new THREE.SphereGeometry(6371, 32, 32)
+var material  = new THREE.MeshLambertMaterial()
 var earthMesh = new THREE.Mesh( geometry, material )
+earthMesh.position.set( 0, 0, 0 )
 scene.add(earthMesh)
 
 // Add Earth Texture
-material.map    = THREE.ImageUtils.loadTexture('Earth/Noclouds.jpg')
+material.map = new THREE.TextureLoader().load("/Earth/Noclouds.jpg")
 
 // Add Bump Map - Produces Height
-material.bumpMap    = THREE.ImageUtils.loadTexture('Earth/Bump.jpg')
-material.bumpScale = 5
+material.bumpMap = new THREE.TextureLoader().load("/Earth/bump.jpg")
+material.bumpScale = 1
 
 // Create Specularity of Sea
-material.specularMap    = THREE.ImageUtils.loadTexture('Earth/Specular.jpg')
+material.specularMap    = new THREE.TextureLoader().load("/Earth/specular.jpg")
 material.specular  = new THREE.Color('grey')
-
-// Add Clouds
-var geometry   = new THREE.SphereGeometry(0.51, 32, 32)
-var material  = new THREE.MeshPhongMaterial({
-  map     : new THREE.Texture(canvasCloud),
-  side        : THREE.DoubleSide,
-  opacity     : 0.8,
-  transparent : true,
-  depthWrite  : false,
-})
-var cloudMesh = new THREE.Mesh(geometry, material)
-earthMesh.add(cloudMesh)
 
 function animate() 
 {
   requestAnimationFrame( animate );
-  earthMesh.rotation.y += 0.001275;
+  earthMesh.rotation.y += 0.001;
   renderer.render( scene, camera );
 }
 animate( );
