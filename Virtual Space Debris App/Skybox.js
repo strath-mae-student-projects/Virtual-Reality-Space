64525122392db150e986171e7209b37515e7d366
KevinCampbel <66116091+KevinCampbel@users.noreply.github.com>
2020-07-06 12:50:32 +0100

    Update Skybox.js

--- Virtual Space Debris App/Skybox.js
+++ Virtual Space Debris App/Skybox.js
@@ -3,7 +3,7 @@ function init() {
   scene = new THREE.Scene;
 
   camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000000);
-  camera.position.set(-90, -20, -90);
+  camera.position.set(-200, 6600, -1800);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
