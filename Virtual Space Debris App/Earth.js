1bf3a018d62c85baa93addfe0ddf327a34b63e7d
KevinCampbel <66116091+KevinCampbel@users.noreply.github.com>
2020-07-28 11:30:24 +0100

    Update Earth.js

--- Virtual Space Debris App/Earth.js
+++ Virtual Space Debris App/Earth.js
@@ -14,6 +14,17 @@ var earthMesh = new THREE.Mesh( geometry, material )
 earthMesh.position.set( 0, 0, 0 )
 scene.add(earthMesh)
 
+// Inside Sphere
+// create the geometry sphere
+var geometry1  = new THREE.SphereGeometry(6371, 32, 32)
+// create the material, using a texture of startfield
+var material1  = new THREE.MeshBasicMaterial()
+material1.map   = THREE.ImageUtils.loadTexture('/Earth/Noclouds.jpg')
+material1.side  = THREE.BackSide
+// create the mesh based on geometry and material
+var mesh  = new THREE.Mesh(geometry1, material1)
+scene.add(mesh)
+
 // Add Earth Texture
 material.map = new THREE.TextureLoader().load("/Earth/Noclouds.jpg")
 
